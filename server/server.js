32ff02850e49cfb74a851f84a6225cfbcc5491de
Iain Floyd <39953553+floyd924@users.noreply.github.com>
2018-11-15 16:15:50 +0000

    tries to add heroku compatibility
    previous commit did not

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(parser.json());
 //   })
 //   .catch(console.err);
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, function () {
   console.log(`Listening on port ${ this.address().port }`);
